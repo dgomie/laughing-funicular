07661c442cd569aadf656ef30c8f74e1a736e613
Danny <danny.f.gomez@gmail.com>
2024-06-06 13:01:11 -0400

    add user routes using student routes example

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getStudents,
+  getSingleStudent,
+  createStudent,
+  deleteStudent,
+  addAssignment,
+  removeAssignment,
+} = require('../../controllers/studentController');
+
+// /api/students
+router.route('/').get(getStudents).post(createStudent);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleStudent).delete(deleteStudent);
+
+// /api/students/:studentId/assignments
+router.route('/:userId/thought').post(addAssignment);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route('/:userId/thought/:thoughtId').delete(removeAssignment);
+
+module.exports = router;
