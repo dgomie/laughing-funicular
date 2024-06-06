386846aed92645c01c5cd813ee7c73bc3b6b0541
Danny <danny.f.gomez@gmail.com>
2024-06-06 14:36:52 -0400

    created userSchema with required fields

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+      unique: true,
+      trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+          validator: function (v) {
+            return emailRegex.test(v);
+          },
+          message: props => `${props.value} is not a valid email address!`
+        }
+      },
+    thoughts: [thoughtSchema],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+  })
+
+const Student = model('student', studentSchema);
+
+module.exports = Student;
