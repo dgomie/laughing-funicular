b97cf71d2057eebc956e871957a5911ef761b79a
Danny <danny.f.gomez@gmail.com>
2024-06-06 14:44:55 -0400

    created thoughtSchema with required fields

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const userSchema = require('./User')
+
+const userSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+  }
+);
+
+userSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+  })
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
