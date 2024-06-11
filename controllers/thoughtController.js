8ea8325fe991f7292e0d5d324b2ce2c1b4059c0e
Danny <danny.f.gomez@gmail.com>
2024-06-11 18:22:34 -0400

    began removeReaction controller and route. Still debugging

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -80,4 +80,47 @@ module.exports = {
     }
   },
 
+  async addReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      console.log(thought)
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: 'No thought found with that ID :(' })
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+//   // Remove reaction from a thought
+  async removeReaction(req, res) {
+    try {
+
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: req.params.reactionId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: 'No thought found with that ID :(' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
 };
--- models/Reaction.js
+++ models/Reaction.js
@@ -17,6 +17,10 @@ const reactionSchema = new Schema(
         type: String,
         required: true
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
   },
   {
     toJSON: {
