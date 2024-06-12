453a6b23f1923b07a45f7c370a4dadc48ff04788
Danny <danny.f.gomez@gmail.com>
2024-06-12 00:14:31 -0400

    associated thoughts to users

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongoose').Types;
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 
 module.exports = {
@@ -40,7 +40,19 @@ module.exports = {
   async createThought(req, res) {
     try {
       const thought = await Thought.create(req.body);
-      res.json(thought);
+  
+      // Find the user by ID and update their thoughts array
+      const user = await User.findByIdAndUpdate(
+        req.body.userId, // The ID of the user to update
+        { $push: { thoughts: thought._id } }, // Add the ID of the new thought to the user's thoughts array
+        { new: true, runValidators: true } // Return the updated user and run the schema validators
+      );
+  
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with this id!' });
+      }
+  
+      res.json({ thought, user });
     } catch (err) {
       res.status(500).json(err);
     }
